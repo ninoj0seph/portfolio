7063c7d6ce3fb4270048fbb8610dde42c3221709
ninoj0seph <ninojoseph.tugade@gmail.com>
2017-05-18 23:40:36 -0700

    changed folder routing added resume

--- php/mail_handler.php
+++ php/mail_handler.php
@@ -1,53 +1,18 @@
 <?php
+$name = $_POST['name'];
+$email = $_POST['email'];
+$subject = $_POST['subject'];
+$from = 'From: Your Portfolio';
+$to = 'ninojoseph.tugade@gmail.com';
+$message = $_POST['message'];
 
-/* Configuration */
-/*your web-mail*/
-$mailto  = 'ninojoseph.tugade@gmail.com';
+$body = "From: $name\n E-Mail: $email\n Message:\n $message";
 
-
-$name     	= strip_tags($_POST['first_name']);
-$sub     	= strip_tags($_POST['sub']);
-$email      = strip_tags($_POST['email']);
-$comments   = strip_tags($_POST['message']);
-
-$subject = "[Contact Form]";
-
-// HTML for email to send submission details
-$body = "
-<br>
-<p><b>Message</b>: $comments</p>
-<p><b>Name</b>: $name <br>
-<p><b>Subject</b>: $sub <br>
-<b>Email</b>: $email<br>
-";
-
-
-// Success Message
-$success = "Sent";
-$error = "Failed";
-
-
-$headers = "From: $name <$email> \r\n";
-$headers .= "Reply-To: $email \r\n";
-$headers .= "MIME-Version: 1.0\r\n";
-$headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-$headers2 = "From:" . $mailto;
-$message = "<html><body>$body</body></html>";
-
-if(empty($name) || empty($sub) || empty($email) || empty($comments) ){
-
-    echo "Fill The Form Properly"; // message
-    header("Location: http://localhost:8888/index-normal.html"); //go to home page
-    die();
-
-}
-
-else {
-
-    if (mail($mailto, $subject, $message, $headers)) {
-        echo "$success"; // success
+if ($_POST['submit']) {
+    if (mail ($to, $subject, $body, $from)) {
+        echo '<p>Your message has been sent!</p>';
     } else {
-        echo "$error"; // failure
+        echo '<p>Something went wrong, go back and try again!</p>';
     }
 }
-
+?>
